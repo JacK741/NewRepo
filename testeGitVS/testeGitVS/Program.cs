2224eebeb6c8240ce56b224822bb10f90ed436e6
Kaue Bastos <kaue-1337@hotmail.com>
2020-10-20 17:00:10 -0300

    commit de teste

---
+++ testeGitVS/testeGitVS/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testeGitVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exemplo de uso da integração do GitHub no VisualStudio");
+            Console.ReadKey();
+
+            //alteração 1
+        }
+    }
+}
